710b4ecf2e86e0a46b050d3fab3786f0d9bcadbe
John-macdonald1 <146704895+John-macdonald1@users.noreply.github.com>
2024-03-04 19:13:33 +1300

    Update escortify.py
    added classes

--- escortify.py
+++ escortify.py
@@ -3,44 +3,50 @@ from discord_webhook import DiscordWebhook
 from selenium.webdriver.common.by import By
 import time
 
-URL = 'https://escortify.co.nz/auckland-escorts/chanelley-25434'
-DISCORD_URL = "https://discord.com"
-ESCORT_NAME = 'CHANELLEY'
+URL = 'https://escortify.co.nz/wellington-escorts/alicia-23780' 
+DISCORD_URL = "https://discord.com/api/webhooks/1214092858085343232/RuZkmA2DyJ6zLMXsYh57PDI6A9IDBT55UPUAFsOCHGyf9BppsmhhM7lrUNdhu_3PcA6L"
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-last_online = None
-initial_likes = None
+class escortmonitor:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--headless')
+        self.last_online = None
+        self.initial_likes = None
 
-current_time = time.time()
-formatted_time = time.strftime("%b %d %a at %I:%M %p", time.localtime(current_time))
-
-while True:
-    driver = webdriver.Chrome(options=options)
-    driver.get(URL)
-    time.sleep(4) 
-    status = driver.find_element(By.XPATH, '//*[@id="contact"]/div[2]/p/span[2]/span').text
-    like_button = driver.find_element(By.XPATH, '//*[@id="voting_plugin"]/div/a/span').text
-
-    if last_online != status:   
-        if status == "OFFLINE":
-            message = f"{ESCORT_NAME} is now offline - {formatted_time}"
-        else:
-            message = f"{ESCORT_NAME} is now 😍🥰online🥰😍 - {formatted_time}"
-
-        webhook_url = DISCORD_URL
-        webhook = DiscordWebhook(url=webhook_url, content=message)
-        response = webhook.execute()
-    last_online = status
+    def get_formatted_time (self):
+        current_time = time.time()
+        return time.strftime("%b %d %a at %I:%M %p", time.localtime(current_time))
     
+    def send_discord_message(self,content):
+        webhook = DiscordWebhook(url=DISCORD_URL, content=content)
+        webhook.execute()
+ 
+    def escort_monitor(self):
+        while True:
+            driver = webdriver.Chrome(options=self.options)
+            driver.get(URL)
+            time.sleep(4) 
+            status = driver.find_element(By.XPATH, '//*[@id="contact"]/div[2]/p/span[2]/span').text 
+            like_button = driver.find_element(By.XPATH, '//*[@id="voting_plugin"]/div/a/span').text
+            escort_name = driver.find_element(By.XPATH, '//*[@id="xxx"]/h1').text
+            current_likes = like_button
 
-    current_likes = like_button
-    if initial_likes is None:
-        initial_likes = current_likes
-    else:
-        if current_likes > initial_likes:
-            like_message = f"New like added for {ESCORT_NAME} at {formatted_time}! Total likes: {current_likes}"
-            like_webhook = DiscordWebhook(url=DISCORD_URL, content=like_message)
-            like_webhook.execute()
+            if self.last_online != status:   
+                if status == "OFFLINE":
+                    message = f"{escort_name} is now offline - {self.get_formatted_time()}"
+                else:
+                    message = f"{escort_name} is now online - {self.get_formatted_time()}"
+                self.send_discord_message(message)
+            self.last_online = status
 
-    time.sleep(500)
+            if self.initial_likes is None:
+                self.initial_likes = current_likes
+            elif current_likes > self.initial_likes:
+                like_message = f"New like added for {escort_name} at {self.get_formatted_time()}! Total likes: {current_likes}"
+                self.send_discord_message(like_message)
+            self.initial_likes = current_likes
+
+            time.sleep(600)
+
+monitor = escortmonitor()
+monitor.escort_monitor()
